55a26f664f5e8b4e02a7a54616ffd7bd8e2a4df0
Anandita <rizkianandita6@gmail.com>
2023-02-16 15:31:38 +0700

    jwt auth

---
+++ src/main/kotlin/com/rrlabs/auth/JwtService.kt
@@ -0,0 +1,26 @@
+package com.rrlabs.auth
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.rrlabs.data.model.User
+
+class JwtService {
+
+    private val issuer = "noteServer"
+    private val jwtSecret = System.getenv("JWT_SECRET")
+    private val algorithm = Algorithm.HMAC512(jwtSecret)
+
+    val verifier: JWTVerifier = JWT
+        .require(algorithm)
+        .withIssuer(issuer)
+        .build()
+
+    fun generateToken(user: User): String {
+        return JWT.create()
+            .withSubject("NoteAuth")
+            .withIssuer(issuer)
+            .withClaim("email", user.email)
+            .sign(algorithm)
+    }
+}
--- src/main/kotlin/com/rrlabs/plugins/Routing.kt
+++ src/main/kotlin/com/rrlabs/plugins/Routing.kt
@@ -1,13 +1,33 @@
 package com.rrlabs.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
+import com.rrlabs.auth.JwtService
+import com.rrlabs.auth.hash
+import com.rrlabs.data.model.User
+import com.rrlabs.repository.Repo
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    val db = Repo()
+    val jwtService = JwtService()
+    val hashFunction = { s: String -> hash(s) }
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/token") {
+            val email = call.request.queryParameters["email"] ?: ""
+            val name = call.request.queryParameters["name"] ?: ""
+            val password = call.request.queryParameters["password"] ?: ""
+
+            val user = User(
+                email, name, hashFunction(password)
+            )
+            call.respond(jwtService.generateToken(user))
         }
     }
 }
